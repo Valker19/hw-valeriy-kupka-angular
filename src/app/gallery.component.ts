92712de341ae7d96da13deea768ba4d1c2a51758
soulr1ver <dovgal_maxim@mail.ru>
2019-08-11 02:14:18 +0300

    examples

--- src/app/gallery.component.ts
+++ src/app/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { fromEvent } from 'rxjs';
 import { bufferCount } from 'rxjs/operators';
 
@@ -7,22 +7,28 @@ import { bufferCount } from 'rxjs/operators';
     templateUrl: './gallery.component.html'
 })
 
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements AfterViewInit {
+    @ViewChild('btn', { static: false })
+    btn: ElementRef;
+
     imgSrc: string = 'https://s.0564.ua/section/cataloglogo/upload/images/catalog/000/001/260/bez-nazvania_5c53e1f6845c0.jpg';
     buttonText: string = 'Click!';
 
-    constructor() { }
+    // and you must change life cycle hook, because reference to the element will appear only when the dom is raised 
+    ngAfterViewInit(): void {
+        this.buttonClick();
+    }
 
     buttonClick(): void {
-        const publisher = fromEvent(document.querySelector('button'), 'click')
+        // or you can do the same with the viewchild decorator
+        // sure it should be better, if we talk about angular application
+        const publisher = fromEvent(this.btn.nativeElement, 'click')
             .pipe(bufferCount(4));
 
+        // dont't forget call unsubscribe method of the subscriber on ondestroy hook
+        // but in this case it's not necessary
         const subscriber = publisher.subscribe(() => {
             alert('Button pressed 4 times');
         })
-    }
-
-    ngOnInit() {
-        this.buttonClick();
     }
 }
