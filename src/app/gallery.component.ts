5dff68c1acfde0203756d43b25d39e063f4f9690
Styler19 <mr.styler2018@gmail.com>
2019-08-12 11:42:00 +0300

    fix hw6

--- src/app/gallery.component.ts
+++ src/app/gallery.component.ts
@@ -7,12 +7,14 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 
 export class GalleryComponent {
-    @Input()    
-        public imgSrc1: string = 'https://luckypony.co.za/wp-content/uploads/2011/07/img_9786-480x480-300x300.jpg';
-        public imgSrc2: string = 'https://raidline.com/image/cache/catalog/shop/CATEGORIES/Overwatch_RaidLine.com_Boost_OW-480x480.jpg';
-        public imgSrc3: string = 'https://www.buubble.com/wp-content/uploads/2018/01/instagram-iimage-uai-480x480.jpg';
-        public imgSrc4: string = 'http://park72.ru/wp-content/uploads/2019/03/30751965-g1sZbXU2Qu21PboY71IBA__vbF7Ok3nZFDXnx0-OGIk-1538338732-728-27e63c047f-1547377195-480x480.jpg';
-    
+    @Input()
+        public imagesSrc: Object = {
+            imgSrc1: 'https://zapadbaltobuv.ru/image/cache/no_image-210x210.png',
+            imgSrc2: 'https://zapadbaltobuv.ru/image/cache/no_image-210x210.png',
+            imgSrc3: 'https://zapadbaltobuv.ru/image/cache/no_image-210x210.png',
+            imgSrc4: 'https://zapadbaltobuv.ru/image/cache/no_image-210x210.png'
+        }
+
     @Output()
         public selectedImg = new EventEmitter();
 
