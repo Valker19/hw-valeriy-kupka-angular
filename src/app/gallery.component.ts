c27cf26144c753a6e7d2a70d0e0e60165ae701e7
Styler19 <mr.styler2018@gmail.com>
2019-08-14 11:46:34 +0300

    fix hw6

--- src/app/gallery.component.ts
+++ src/app/gallery.component.ts
@@ -8,12 +8,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 export class GalleryComponent {
     @Input()
-        public imagesSrc: Object = {
-            imgSrc1: 'https://zapadbaltobuv.ru/image/cache/no_image-210x210.png',
-            imgSrc2: 'https://zapadbaltobuv.ru/image/cache/no_image-210x210.png',
-            imgSrc3: 'https://zapadbaltobuv.ru/image/cache/no_image-210x210.png',
-            imgSrc4: 'https://zapadbaltobuv.ru/image/cache/no_image-210x210.png'
-        }
+        public imagesSrc: Array<string>;
 
     @Output()
         public selectedImg = new EventEmitter();
