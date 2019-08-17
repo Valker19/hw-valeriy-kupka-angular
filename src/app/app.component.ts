0d288834a03ba54a35ecf6f15394811f44564823
Styler19 <mr.styler2018@gmail.com>
2019-08-17 14:40:15 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoadImagesService } from './loadImages.service';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-gallery></app-gallery>
+    <img *ngFor='let image of images' [src]='image' width='200' height='200'>
   `
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public images: Array<string>;
+  
+  constructor(public service: LoadImagesService) {}
 
+  ngOnInit(): void {
+    this.images = this.service.images;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { GalleryComponent } from './gallery.component';
 import { LoadImagesService } from './loadImages.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GalleryComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
